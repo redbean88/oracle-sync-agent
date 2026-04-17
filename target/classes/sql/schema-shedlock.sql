2c97ee78deb3f87b9752d562c655b4a49d7d92b9
robert.choi <robert.choi@gmail.com>
2026-04-17 16:51:58 +0900

    rm test2

--- target/classes/sql/schema-shedlock.sql
+++
@@ -1,7 +0,0 @@
--- ShedLock
-CREATE TABLE shedlock (
-    name       VARCHAR(64)  PRIMARY KEY,
-    lock_until TIMESTAMP    NOT NULL,
-    locked_at  TIMESTAMP    NOT NULL,
-    locked_by  VARCHAR(255) NOT NULL
-);
